6301a878d1c3f26140034d17a6ff34234be494a0
PhoenixOrigamiProgrammer <rechengeschichte@gmail.com>
2024-04-21 16:02:11 +0200

    1 left

--- src/main/java/net/Phoenixorigami/crafting_3d/crafting_3d.java
+++ src/main/java/net/Phoenixorigami/crafting_3d/crafting_3d.java
@@ -1,13 +1,12 @@
+package net.Phoenixorigami.crafting_3d;
+
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-
+import net.minecraftforge.eventbus.api.IEventBus;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(crafting_3d.MOD_ID)
@@ -19,28 +18,13 @@ public class crafting_3d {
     public crafting_3d() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
-
-        ModVillagers.register(modEventBus);
-        ModPaintings.register(modEventBus);
-
-        ModFluids.register(modEventBus);
-        ModFluidTypes.register(modEventBus);
-
-        ModBlockEntities.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-
-        ModRecipes.register(modEventBus);
-        ModEntityTypes.register(modEventBus);
-
-        ModLootModifiers.register(modEventBus);
-
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
 
-        modEventBus.addListener(this::addCreative);
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.log("Hello World");
     }
 
 }
